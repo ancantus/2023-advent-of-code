9c5aac98097d84be0b3fb12382dc7801977345c0
Ryan Estep <rmestep@bex.net>
2023-12-09 09:43:42 +1300

    AOC1: Completed part 2 ("two" -> 2)
    Not happy with .sub retuning a copied slice: but the version of `BigString` I got didn't match online documentation

--- aoc_1/bin/main.ml
+++ aoc_1/bin/main.ml
@@ -36,8 +36,45 @@ let find_last_digit str = convert_digit (string_rfind is_digit str)
 let find_cal_values str = (find_first_digit str, find_last_digit str)
 let calc_cal_value (a, b) = (Option.get a * 10) + Option.get b
 
+(* Handling the diabolical case where "two" -> "2" *)
+let starts_with ~prefix:search str = 
+        let rec inner search len1 str len2 i =
+                if i >= len1 
+                        then true
+                else if i >= len2
+                        then false
+                else if search.[i] <> Bigstring.get str i
+                        then false
+                else inner search len1 str len2 (i+1)
+        in
+        inner search (String.length search) str (Bigstring.length str) 0
+
+let remap_str input output str = if starts_with ~prefix:input str then Some output else None
+let cal_file_remap = [
+        ("one", '1');
+        ("two", '2');
+        ("three", '3');
+        ("four", '4');
+        ("five", '5');
+        ("six", '6');
+        ("seven", '7');
+        ("eight", '8');
+        ("nine", '9');
+]
+let get_remapper map_table str =
+        let remappers = List.map (fun (x, y) -> remap_str x y) map_table in
+        let str_cache = Bigstring.of_string str in
+        let str_len = Bigstring.length str_cache in
+        (fun i -> List.fold_left 
+                (fun acc f -> Option.value (f (Bigstring.sub str_cache i (str_len - i))) ~default:acc)
+                (Bigstring.get str_cache i)
+                remappers)
+let remap_cal_line str =
+        let cal_file_remapper = get_remapper cal_file_remap str in
+        String.init (String.length str) cal_file_remapper
+
 (* Parsing logic for multi-line *)
 let accum_calc_values acc str =
-        acc + (calc_cal_value (find_cal_values str))
+        acc + (calc_cal_value (find_cal_values (remap_cal_line str)))
 let () = Printf.printf "Calibration Value: %i\n" (In_channel.fold_lines accum_calc_values 0 stdin)
 
