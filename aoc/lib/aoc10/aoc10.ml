c2d39e74f51dc7a951a96d04694d3f777f5d321d
Ryan Estep <rmestep@bex.net>
2023-12-26 14:34:14 +1300

    AOC10: Completed part 2
    Solution ended up being very messy and didn't solve it in a very
    functional way.
    
    Was struggling with the solution and got a bit frustrated. I don't think
    what was produced was very readable or maintainable

--- aoc/lib/aoc10/aoc10.ml
+++ aoc/lib/aoc10/aoc10.ml
@@ -20,7 +20,7 @@ let add_pipe pipe_tbl p =
 let load_map channel =
         let make_map str =
                 let len = String.length str in
-                Bigarray.Array2.create Bigarray.char Bigarray.c_layout len len in
+                Bigarray.Array2.init Bigarray.char Bigarray.c_layout len len (fun _ _ -> '.') in
         let first_line = Option.get (In_channel.input_line channel) in
         let map = make_map first_line in
         let rec load_impl i end_i str =
@@ -38,6 +38,11 @@ let map_coords_seq map =
         let dim2 = Bigarray.Array2.dim2 map in
         Seq.init (dim1 * dim2) (fun i -> (i / dim1, i mod dim2))
 
+let map_row_seq map =
+        let dim1 = Bigarray.Array2.dim1 map in
+        Seq.init dim1 (fun i -> Bigarray.Array2.slice_left map i)
+
+
 let in_map_range map coords =
         let dim1 = Bigarray.Array2.dim1 map in
         let dim2 = Bigarray.Array2.dim2 map in
@@ -52,6 +57,8 @@ let make_pipe symbol_map coords =
         let symbol = symbol_map.{fst coords, snd coords} in
         match symbol with
                 | '.' -> None
+                | 'I' -> None
+                | 'O' -> None
                 | _ -> Some { coords; symbol; visited=false; connections=[]}
 
 let get_pipe symbol_map pipes coords = 
@@ -108,29 +115,83 @@ let make_pipe_graph symbol_map =
                 | Some p -> if p.symbol = 'S' then p :: start_pipes else start_pipes)
                 [] pipe_graph
 
-let find_longest_loop start_pipes =
-        let next_pipes start =
-                List.filter_map (fun p -> if (start.coords = p.coords) || p.visited
-                        then None else Some p) start.connections
+let find_longest_loop start_pipe =
+        let depth_first_search start =
+                let rec inner visited_tbl pipe path =
+                        let visited = Option.value (Coord_tbl.find_opt visited_tbl pipe.coords) ~default:false in
+                        let is_start = pipe.coords = start.coords in
+                        match is_start, visited with
+                                | true, _ -> List.iter print_pipe (pipe :: path); print_endline "Some"; Some path 
+                                | false, true -> List.iter print_pipe (pipe :: path); print_endline "None"; None 
+                                | false, false -> Coord_tbl.add visited_tbl pipe.coords true;
+                                        let possible_loops = List.filter_map 
+                                                (fun p -> if p.coords = pipe.coords then None else
+                                                        if p.coords = (List.hd path).coords then None else
+                                                        inner visited_tbl p ( pipe :: path)) pipe.connections in
+                                        let long_loop = List.fold_left 
+                                                (fun acc l -> if List.length l > List.length acc 
+                                                        then l else acc) [] possible_loops in
+                                        if List.is_empty long_loop 
+                                                then None
+                                                else Some long_loop
+                in
+                List.find_map (fun p -> 
+                        let visited_tbl = Coord_tbl.create 1 in inner visited_tbl p [start]) start.connections
         in
-        let rec count_loops pipes steps loop_counts =
-                let next_pipes = List.fold_left (fun acc p -> (next_pipes p) @ acc) [] pipes in
-                List.iter (fun p -> p.visited <- true) next_pipes;
-                match next_pipes with
-                        | _ :: [] -> loop_counts 
-                        | [] -> loop_counts
-                        | head :: tail -> if List.for_all (fun p -> head.coords = p.coords) tail 
-                                then (steps + 1) :: loop_counts
-                                else count_loops next_pipes (steps + 1) loop_counts
+        depth_first_search start_pipe
+
+let count_inclosed_tiles loop =
+        let max_dims = List.fold_left (fun acc p -> match acc, p.coords with
+                | (acc_x, acc_y), (p_x, p_y) -> (max acc_x p_x, max acc_y p_y)) (0, 0) loop in
+        let wind_array = Bigarray.Array2.init 
+                Bigarray.int Bigarray.c_layout ((fst max_dims) + 1) ((snd max_dims) + 1) (fun _ _ -> min_int) in
+        let get_winding back p front = 
+                let back_y = (fst p.coords) - (fst back.coords) in 
+                if back_y > 0 then -1
+                else if back_y < 0 then 1
+                else let front_y = (fst p.coords) - (fst front.coords) in
+                        if front_y > 0 then 1 
+                        else if front_y < 0 then -1
+                        else 0
         in
-        List.iter (fun p -> p.visited <- true) start_pipes;
-        let loop_counts = count_loops start_pipes 0 [] in
-        List.fold_left max 0 loop_counts
+        let rec set_wind_array start pipes =
+                match pipes with
+                        | [] -> ()
+                        | _ :: [] -> ()
+                        | back :: p :: [] ->
+                                        wind_array.{fst p.coords, snd p.coords} <- get_winding back p start;
+                                        wind_array.{fst start.coords, snd start.coords} <- 
+                                                get_winding p start (List.nth loop 1) 
+                        | back :: p :: front :: tail -> 
+                                        wind_array.{fst p.coords, snd p.coords} <- get_winding back p front;
+                                        set_wind_array start (p :: front :: tail)
+        in
+        set_wind_array (List.hd loop) loop;
+        Seq.iter (fun row -> for i = 0 to (Bigarray.Array1.dim row) - 1 do
+                        if row.{i} = 1 then print_char 'U' 
+                        else if row.{i} = -1 then print_char 'D'
+                        else if row.{i} = 0 then print_char '-'
+                        else print_char '.'
+                  done; print_endline "" ) (map_row_seq wind_array);
+        let rec inner_count row i last_cross winding count =
+                let dim = Bigarray.Array1.dim row in
+                if i >= dim then count 
+                else if row.{i} = 0 then inner_count row (i+1) last_cross winding count
+                else if row.{i} = last_cross then inner_count row (i+1) last_cross winding count 
+                else if row.{i} = min_int then (
+                        let count = if winding = 0 then count else count + 1 in
+                        inner_count row (i+1) last_cross winding count
+                )
+                else inner_count row (i+1) row.{i} (winding + row.{i}) count
+        in
+        Seq.fold_left (fun tiles row -> tiles + (inner_count row 0 0 0 0)) 0 (map_row_seq wind_array)
 
 let run () =
         let map = load_map stdin in
-        let start_pipes = make_pipe_graph map in
-        let loop_count = find_longest_loop start_pipes in
-        Printf.printf "Longest pipe distance: %d\n" loop_count
+        let start_pipe = List.hd (make_pipe_graph map) in
+        let longest_loop = List.rev (Option.value (find_longest_loop start_pipe) ~default:[]) in
+        Printf.printf "Longest pipe distance: %d\n" ((List.length longest_loop) / 2);
+        let inclosed_tile_count = count_inclosed_tiles longest_loop in
+        Printf.printf "Inclosed tile count: %d\n" inclosed_tile_count  
 
 
